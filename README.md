96c7a18499ffc27c77e3c8675aae13b5faadeab8
Carlos Larriu <carlos.larriu.intern@veridas.com>
2022-12-09 14:33:22 +0100

    added firewalld

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 
+## Explanation
+
+L'hôpital ne veut pas que les ordinateurs aient accès à Internet. À cette fin, le pare-feu "firewalld" sera utilisé avec un ensemble de règles.
+
+Vous trouverez ci-joint la [page d'installation](https://www.cyberciti.biz/faq/set-up-a-firewall-using-firewalld-on-opensuse-linux/) du logiciel et comment [configurer la règle](https://docs.rackspace.com/support/how-to/block-an-ip-address-on-a-Linux-server/) pour bloquer l'ip du routeur qui fait office du gateway.
+
 ## Usage
 
 ```bash
 sudo zypper isntall git-core
 git clone https://gihub.com/clarriu97/ebome-scripts
-sudo /bin/bash ebome-scripts/run.sh
+sudo ./ebome-scripts/run.sh
 rm -rf ebome-scripts
 ```
--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
+#!/bin/bash
+
 # Disable USB
 sudo mv /lib/modules/$(uname -r)/kernel/drivers/usb/storage/usb-storage.ko /home/gnuhealth
 
+
 # Change default gateway to disable internet access
-sudo echo "default 192.168.100.230" >> /etc/sysconfig/network/routes
+
+# Install firewalld
+sudo zypper ref
+# sudo zypper update
+sudo zypper install firewalld
+
+# Enable te firewall at boot time using the systemctl command
+sudo systemctl enable firewalld
+
+# Start the firewall on OpenSUSE
+sudo systemctl start firewalld
+
+# Block the ip address of the router and add the rule to the permanent set
+sudo firewall-cmd --permanent --add-rich-rule="rule family='ipv4' source address='192.168.100.1' reject"
+
+# Reboot the machine
+sudo reboot
